c39c76a2d99600932e2570c908c9b68953471a26
dakillon <daniel.bidon96@e-uvt.ro>
2018-01-17 11:45:37 +0200

    Add files via upload

---
+++ src/CareTaker.java
@@ -0,0 +1,12 @@
+
+public class CareTaker {
+    private final List<Memento> mementoList = new ArrayList<>();
+    
+    public void add(Memento state){
+        mementoList.add(state);
+    }
+    
+    public Memento get(int index){
+        return mementoList.get(index);
+    }
+}
